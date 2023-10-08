f9222fa592dc53b188bb89a8cf8424612b93ff8a
Nico <133128777+RustedNature@users.noreply.github.com>
2023-10-08 15:30:37 +0200

    added more tests to asl

--- src/test/cpu_tests/asl_tests.rs
+++ src/test/cpu_tests/asl_tests.rs
@@ -7,4 +7,26 @@ fn asl_accumulator() {
     assert_eq!(cpu.accumulator, 0b0001_1110);
     assert_ne!(cpu.status, NEGATIVE_FLAG);
     assert_ne!(cpu.status, ZERO_FLAG);
+    assert_ne!(cpu.status, CARRY_FLAG);
 }
+#[test]
+fn asl_set_carry_flag(){
+    let mut cpu = CPU::default();
+    cpu.load_and_run(vec![0xa9, 0b1000_1111, 0x0a, 0x00]);
+    assert_eq!(cpu.accumulator, 0b0001_1110);
+    assert_eq!(cpu.status, CARRY_FLAG);
+}
+#[test]
+fn asl_set_zero_flag(){
+    let mut cpu = CPU::default();
+    cpu.load_and_run(vec![0xa9, 0b0000_0000, 0x0a, 0x00]);
+    assert_eq!(cpu.accumulator, 0b0000_0000);
+    assert_eq!(cpu.status, ZERO_FLAG);
+}
+ #[test]
+ fn asl_set_negative_flag() {
+    let mut cpu =CPU::default();
+    cpu.load_and_run(vec![0xa9, 0b0100_1111, 0x0a, 0x00]);
+    assert_eq!(cpu.accumulator, 0b1001_1110);
+    assert_eq!(cpu.status, NEGATIVE_FLAG);
+ }
